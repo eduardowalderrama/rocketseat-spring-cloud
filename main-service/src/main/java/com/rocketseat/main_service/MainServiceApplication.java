3a40903524bba03663a8a634b04a7993f2cac890
eduardowalderrama <walderramaeduardo@gmail.com>
2025-03-16 15:12:24 -0300

    configuracao centralizada com spring cloud config - implementacao do spring cloud config server

--- main-service/src/main/java/com/rocketseat/main_service/MainServiceApplication.java
+++ main-service/src/main/java/com/rocketseat/main_service/MainServiceApplication.java
@@ -2,8 +2,12 @@ package com.rocketseat.main_service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
+@EnableEurekaServer
+@EnableConfigServer
 public class MainServiceApplication {
 
 	public static void main(String[] args) {
