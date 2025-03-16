2033bda4aaee627eb33ce576e07fb6ed1f84a511
eduardowalderrama <walderramaeduardo@gmail.com>
2025-03-16 14:58:18 -0300

    introducao ao spring cloud - instalacao e configuracao do ambiente

---
+++ main-service/src/test/java/com/rocketseat/main_service/MainServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rocketseat.main_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MainServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
