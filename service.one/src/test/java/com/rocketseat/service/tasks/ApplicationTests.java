d7f795fe5614ea4808363d721595856604da7431
eduardowalderrama <walderramaeduardo@gmail.com>
2025-03-18 21:16:41 -0300

    comunicacao entre microservicos com feign e resttemplate - comecando a configuracao do nosso primeiro microservico

--- service.one/src/test/java/com/rocketseat/service/tasks/ApplicationTests.java
+++ service.one/src/test/java/com/rocketseat/service/tasks/ApplicationTests.java
@@ -1,13 +1,8 @@
-package com.rocketseat.service.one;
+package com.rocketseat.service.tasks;
 
-import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class ApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
 
 }
